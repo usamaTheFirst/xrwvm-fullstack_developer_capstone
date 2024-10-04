d258a1a9ed188212ccdf6d82a868ff1da65da21f
usamaTheFirst <usamafiaz1453@gmail.com>
2024-10-04 13:51:57 -0400

    module 3.3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,11 @@ urlpatterns = [
     path('register', views.registration_request, name='register'),
     path(route = 'get_cars/', view = views.get_cars, name = 'getcars'),
     # path for dealer reviews view
-
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
